d22616be43ff51483976328caee254ba5872458f
thq4n <gianquan2000@gmail.com>
2021-06-18 17:07:58 +0700

    ternary expression

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      "questionText": "What's your favorite color?",
+      "answers": ["Black", "Red", "Screen", "White"],
+    },
+    {
+      "questionText": "What's your favorite animal?",
+      "answers": ["Dog", "Cat", "Snake", "Rabbit"],
+    },
+    {
+      "questionText": "What's your favorite drink?",
+      "answers": ["Cock", "Pepsi", "Sting", "7Up"],
+    },
+  ];
+
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
       _questionIndex++;
-      if (_questionIndex > 2) {
-        _questionIndex = 0;
-      }
     });
 
     print(_questionIndex);
@@ -30,35 +42,32 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        "questionText": "What's your favorite color?",
-        "answers": ["Black", "Red", "Screen", "White"],
-      },
-      {
-        "questionText": "What's your favorite animal?",
-        "answers": ["Dog", "Cat", "Snake", "Rabbit"],
-      },
-      {
-        "questionText": "What's your favorite drink?",
-        "answers": ["Cock", "Pepsi", "Sting", "7Up"],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My first app"),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(
-                questions.elementAt(_questionIndex)['questionText'].toString()),
-            ...(questions[_questionIndex]["answers"] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList(),
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: <Widget>[
+                  Question(questions
+                      .elementAt(_questionIndex)['questionText']
+                      .toString()),
+                  ...(questions[_questionIndex]["answers"] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList(),
+                ],
+              )
+            : Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    _questionIndex = -1;
+                    _answerQuestion();
+                  },
+                  child: Text("Try again!"),
+                ),
+              ),
       ),
     );
   }
